d2dd79cf86364c88ffe5f68d81fd1fc76fed9a54
15zed <108381807+15zed@users.noreply.github.com>
2024-06-04 10:02:24 +0800

    Update RegistryServiceCache.java 消费者本地缓存改用Map

--- RPC-starter/src/main/java/org/example/rpcstarter/registry/RegistryServiceCache.java
+++ RPC-starter/src/main/java/org/example/rpcstarter/registry/RegistryServiceCache.java
@@ -3,6 +3,8 @@ package org.example.rpcstarter.registry;
 import org.example.rpcstarter.common.ServiceMetaInfo;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 注册中心服务的本地缓存，让消费者每次不用都从注册中心拉取可用服务
@@ -10,23 +12,26 @@ import java.util.List;
 public class RegistryServiceCache {
     /**
      * 服务缓存
+     * key是服务键名称：GoodsService:1.0  value是服务节点信息
      */
-    List<ServiceMetaInfo> registerServiceCache;
+    Map<String,List<ServiceMetaInfo>> registerServiceCache = new ConcurrentHashMap<>();
 
     /**
      * 设置服务缓存
-     * @param metaInfoList 服务信息列表
+     * @param serviceKey 服务键名
+     * @param serviceMetaInfos 服务信息列表
      */
-    public void setCache(List<ServiceMetaInfo> metaInfoList){
-        this.registerServiceCache = metaInfoList;
+    public void setCache(String serviceKey,List<ServiceMetaInfo> serviceMetaInfos){
+        this.registerServiceCache.put(serviceKey,serviceMetaInfos);
     }
 
     /**
      * 读取服务缓存
+     * @param serviceKey 服务键名
      * @return 服务集合
      */
-    public List<ServiceMetaInfo> readCache(){
-        return this.registerServiceCache;
+    public List<ServiceMetaInfo> readCache(String serviceKey){
+        return this.registerServiceCache.get(serviceKey);
     }
 
     /**
@@ -35,5 +40,4 @@ public class RegistryServiceCache {
     public void clearCache(){
         this.registerServiceCache = null;
     }
-
 }
